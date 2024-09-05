f0577526925c155bbe09a25ddda8fc51d809ee56
Utkarsh Raj Sinha <ur5441@srmist.edu.in>
2024-09-05 14:05:46 +0530

    New: Added Binary Search algorithm

---
+++ Algorithms/Binary Search/Binary SearchwithArray/binary_searchwithArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int binarySearch(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n; // Size of array
+    std::cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    int key;
+    std::cin >> key;
+
+    std::cout << binarySearch(arr, n, key) << std::endl;
+
+    return 0;
+}
