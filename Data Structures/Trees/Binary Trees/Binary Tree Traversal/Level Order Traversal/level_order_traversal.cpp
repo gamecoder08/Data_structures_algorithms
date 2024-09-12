5cae888bec7e798ddf76f1f58d47baaf72bc7809
Utkarsh Raj Sinha <gamecoder0811@gmail.com>
2024-09-12 11:08:23 +0530

    Added Level order traversal and rearranged some files

---
+++ Data Structures/Trees/Binary Trees/Binary Tree Traversal/Level Order Traversal/level_order_traversal.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <queue>
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printlevelOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    std::queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+        if (node != NULL)
+        {
+            std::cout << node->data << " ";
+            if (node->left)
+            {
+                q.push(node->left);
+            }
+            if (node->right)
+            {
+                q.push(node->right);
+            }
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    /*
+       1
+      / \
+     2   3
+    */
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    /*
+               1
+             /   \
+            2     3
+           / \   / \
+          4   5 6   7
+    */
+
+    printlevelOrder(root);
+
+    return 0;
+}
