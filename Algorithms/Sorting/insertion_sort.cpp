5e2d38cf6308a599184dd4240da7fbb7b7427b26
gamecoder08 <gamecoder0811@gmail.com>
2025-12-09 20:06:28 +0530

    Update: added insertion sort

---
+++ Algorithms/Sorting/insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void insertionSort(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {21, 20, 29, 18, 7, 8, 25, 29, 13};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for(int i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    insertionSort(arr, n);
+    std::cout << "Sorted array: ";
+    for(int i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    return 0;
+}
